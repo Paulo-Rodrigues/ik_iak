eace20e2a008817660fa753351d2213004bda3e3
Paulo Rodrigues <paulor2222@gmail.com>
2019-12-22 20:44:37 -0200

    votable module prototype updated

--- app/models/concerns/votable.rb
+++ app/models/concerns/votable.rb
@@ -4,11 +4,32 @@ module Votable
   module Voter
 
     def upvote(votable)
-      votes.create(votable: votable, user_id: self.id, vote_status: :upvote)
+      if voted?(votable)
+        update_vote(votable, :upvote)
+      else
+        create_vote(votable, :upvote)
+      end
     end
 
     def downvote(votable)
-      votes.create(votable: votable, user_id: self.id, vote_status: :downvote)
+      if voted?(votable)
+        update_vote(votable, :downvote)
+      else
+        create_vote(votable, :downvote)
+      end
+    end
+
+    def create_vote(votable, vote_status)
+      votes.create(votable: votable, user_id: self.id, vote_status: vote_status)
+    end
+
+    def update_vote(votable, vote_status)
+      vote = votes.where(votable_id: votable.id, user_id: self.id).first
+      vote.update(vote_status: vote_status)
+    end
+
+    def voted?(votable)
+      votes.where(votable_id: votable.id).exists?
     end
   end
 
